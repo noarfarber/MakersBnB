95908ce7e237deb937de32de5c3b220932fcb041
Mike Jeuga <mikejeuga@gmail.com>
2021-04-28 23:57:09 +0100

    Added the login views.

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,9 @@
+feature "The login page" do
+  scenario "should show a username and password field and Button" do
+    visit "/"
+    click_button("Login")
+    expect(current_path).to eq "/login"
+    expect(page).to have_content("Username:")
+    expect(page).to have_button("Go")
+  end
+end
