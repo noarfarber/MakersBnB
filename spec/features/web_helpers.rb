d05b920e443fcf6376aec81945b34bcd5bd2e9b7
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-29 18:49:07 +0100

    bookings confirmation is picking up user name

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -8,3 +8,10 @@ def visit_home_sign_up
   visit '/'
   click_button "Sign up"
 end
+
+def sign_up_create_user_visit_accommodations_select
+  visit "/signup"
+  click_button("Create user")
+  visit "/accommodations"
+  click_button("Select")
+end 
